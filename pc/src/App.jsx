3c2d20dfbd677b5820a920aa3ffcfdb1239b8cb3
ruirui <ruikeitai23@gmail.com>
2025-02-21 13:54:42 +0900

    add modify 微修正

--- pc/src/App.jsx
+++ pc/src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 import WebSocketConnector from './components/WebSocketConnector.jsx'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route ,Link,Routes} from 'react-router-dom'
 import ModeSelect from './pages/ModeSelect.jsx'
 
 function App() {
 
   return (
     <Router>
+      <h1>AirGuitar</h1>
       <li><Link to='/ModeSelect'>ModeSelect</Link></li>
+      <li><Link to='/WebSocketConnector'>WebSocketConnector</Link></li>
 
       <Routes>
+        <Route path="/" element={<App />} />
         <Route path="/ModeSelect" element={<ModeSelect />} />
+        <Route path="/WebSocketConnector" element={<WebSocketConnector />} />
         {/* <Route path="/xxx" element={<xxx />} /> */}
       </Routes>
     </Router>
