72610795cbc93e4abea3864e9ebc608bc30600bb
Gypsophila1912 <noma.19.anko@gmail.com>
2025-02-23 08:26:21 +0900

    Add 音設定画面作成・音保存

--- pc/src/components/ModeButton.jsx
+++ pc/src/components/ModeButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ModeButton = ({buttonName}) => {
+const ModeButton = ({buttonName,onClick}) => {
     const sendMessage = () => {
         if (ws && ws.readyState === WebSocket.OPEN) {
             console.log('メッセージ送信中...');
@@ -10,8 +10,9 @@ const ModeButton = ({buttonName}) => {
             console.log('WebSocketが接続されていません');
         }
     }
+    const handleClick = onClick || sendMessage;
 return (
-    <button className='bg-white shadow-md rounded-lg overflow-hidden' onClick={sendMessage}>
+    <button className='bg-white shadow-md rounded-lg overflow-hidden' onClick={handleClick}>
         <div className='bg-blue-500 text-white p-4'>
             {buttonName}
         </div>
