6d4b4632dd8806726565fc26e58d3a11475565d8
Gypsophila1912 <noma.19.anko@gmail.com>
2025-02-23 18:17:51 +0900

    Add 演奏画面レイアウト作成

--- pc/src/pages/PlayPage.jsx
+++ pc/src/pages/PlayPage.jsx
@@ -1,6 +1,10 @@
 import React, {useState,useEffect} from 'react'
 import Preparation from './Preparation'
 import { useLocation } from 'react-router-dom'
+import myImage from '../assets/anpu.png';
+import myImage2 from '../assets/anpupegu.png';
+import myImage3 from '../assets/me-ta-.jpg';
+import background from '../assets/backgroundpicture.png';
 
 const PlayPage = () => {
     const location = useLocation();
@@ -26,7 +30,22 @@ const PlayPage = () => {
             <>
             {/* // プレイ中画面 */}
                 <h1>Play画面</h1>
-                <button onClick={() => setIsPlaying(false)}>Stop</button>
+                <div><button onClick={() => setIsPlaying(false)}>Stop</button></div>
+                <div style={{backgroundImage: `url(${background})`,backgroundSize: 'cover',backgroundPosition: 'left',height: '100vh',}}>
+                    <div>
+                        <img src={myImage3} style={{display: 'block',marginLeft: 'auto',marginRight: 'auto',width :'1500px', height: '200px',marginBottom: '40px'}} />
+                        <button>
+                            <img src={myImage2} style={{ width: '130px', height: '130px', marginRight: '30px' ,marginBottom: '40px'}} />
+                        </button>
+                        <button>
+                            <img src={myImage2} style={{ width: '130px', height: '130px', marginRight: '30px' ,marginBottom: '40px'}} />
+                        </button>
+                        <button>
+                            <img src={myImage2} style={{ width: '130px', height: '130px', marginRight: '30px' ,marginBottom: '40px'}} />
+                        </button>
+                    </div>
+                    <img src={myImage} style={{display: 'block',marginLeft: 'auto',marginRight: 'auto',width: '78%',height: 'auto'}}/>
+                </div>
             </>
         ):(<></>)}
             {/* // 準備画面 */}
