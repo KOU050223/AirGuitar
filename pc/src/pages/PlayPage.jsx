ba35d179b5ba8052df809ebf09a38f0d5ac641e1
Gypsophila1912 <noma.19.anko@gmail.com>
2025-02-23 22:35:03 +0900

    Add 演奏画面背景アニメーション

--- pc/src/pages/PlayPage.jsx
+++ pc/src/pages/PlayPage.jsx
@@ -1,10 +1,11 @@
-import React, {useState,useEffect} from 'react'
-import Preparation from './Preparation'
-import { useLocation } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import Preparation from './Preparation';
+import { useLocation } from 'react-router-dom';
 import myImage from '../assets/anpu.png';
 import myImage2 from '../assets/anpupegu.png';
-import myImage3 from '../assets/me-ta-.jpg';
-import background from '../assets/backgroundpicture.png';
+import videoFile from '../assets/guitarman_animation.mp4'; // 動画をインポート
+import videoFile2 from '../assets/me-ta-_animation.mp4'; // 動画をインポート
+import '../PlayPage.css'; // スタイルシートをインポート
 
 const PlayPage = () => {
     const location = useLocation();
@@ -18,43 +19,54 @@ const PlayPage = () => {
           setData(location.state);
           setMode(location.state.mode);
           console.log(location.state);
-          console.log('MODE:',location.state.mode);
-        }else {
+          console.log('MODE:', location.state.mode);
+        } else {
             window.location.href = '/modeselect';
         }
-      }, [location.state]);
-    
-  return (
-    <>
-        {isPlaying ? (
-            <>
-            {/* // プレイ中画面 */}
-                <h1>Play画面</h1>
-                <div><button onClick={() => setIsPlaying(false)}>Stop</button></div>
-                <div style={{backgroundImage: `url(${background})`,backgroundSize: 'cover',backgroundPosition: 'left',height: '100vh',}}>
+    }, [location.state]);
+
+    return (
+        <>
+            {isPlaying ? (
+                <>
+                {/* プレイ中画面 */}
+                <div className="App">
+                    <div className="video-background">
+                        <video autoPlay loop muted style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: -1 }}>
+                            <source src={videoFile} type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                </div>
+                <div className="content">
+                    <h1 className="title">Play画面</h1>
+                    <div className="title"><button onClick={() => setIsPlaying(false)}>Stop</button></div>
                     <div>
-                        <img src={myImage3} style={{display: 'block',marginLeft: 'auto',marginRight: 'auto',width :'1500px', height: '200px',marginBottom: '40px'}} />
+                    <video autoPlay loop muted style={{width: '3000px', height: '240px', objectFit: 'cover', zIndex: -1 }}>
+                            <source src={videoFile2} type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
                         <button>
-                            <img src={myImage2} style={{ width: '130px', height: '130px', marginRight: '30px' ,marginBottom: '40px'}} />
+                            <img src={myImage2} style={{ width: '130px', height: '130px', marginRight: '30px', marginBottom: '43px' }} />
                         </button>
                         <button>
-                            <img src={myImage2} style={{ width: '130px', height: '130px', marginRight: '30px' ,marginBottom: '40px'}} />
+                            <img src={myImage2} style={{ width: '130px', height: '130px', marginRight: '30px', marginBottom: '43px' }} />
                         </button>
                         <button>
-                            <img src={myImage2} style={{ width: '130px', height: '130px', marginRight: '30px' ,marginBottom: '40px'}} />
+                            <img src={myImage2} style={{ width: '130px', height: '130px', marginRight: '30px', marginBottom: '43px' }} />
                         </button>
                     </div>
-                    <img src={myImage} style={{display: 'block',marginLeft: 'auto',marginRight: 'auto',width: '78%',height: 'auto'}}/>
+                    <img src={myImage} style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', width: '100%', height: 'auto' }} />
                 </div>
-            </>
-        ):(<></>)}
-            {/* // 準備画面 */}
+                </>
+            ) : (<></>)}
+            {/* 準備画面 */}
             <Preparation
                 mode={mode}
                 setIsPlaying={setIsPlaying}
             />
-    </>
-  )
+        </>
+    );
 }
 
-export default PlayPage
+export default PlayPage;
